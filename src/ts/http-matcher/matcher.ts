9b6204d592e76bbd80eaf62e61682aee8b0c6ab8
Wenhao Ni <niwenhao@gmail.com>
2018-05-31 00:04:10 +0900

    u

--- src/ts/http-matcher/matcher.ts
+++ src/ts/http-matcher/matcher.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express"
+import * as jsonQuery from 'json-query'
+
 export interface Matcher<I> {
-    matches(org: I): boolean
+    matches(input: I): boolean
 }
 
 export class AndMatcher<I> implements Matcher<I> {
@@ -10,7 +12,7 @@ export class AndMatcher<I> implements Matcher<I> {
         this.matchers = matchers
     }
 
-    matches = (org: I) => this.matchers.reduce<boolean>((result, matcher) => result && matcher.matches(org), true)
+    matches = (input: I) => this.matchers.reduce<boolean>((result, matcher) => result && matcher.matches(input), true)
 }
 
 export class OrMatcher<I> implements Matcher<I> {
@@ -20,7 +22,7 @@ export class OrMatcher<I> implements Matcher<I> {
         this.matchers = matchers
     }
 
-    matches = (org: I) => this.matchers.reduce<boolean>((result, matcher) => result || matcher.matches(org), false)
+    matches = (input: I) => this.matchers.reduce<boolean>((result, matcher) => result || matcher.matches(input), false)
 }
 
 export class NotMatcher<I> implements Matcher<I> {
@@ -30,10 +32,34 @@ export class NotMatcher<I> implements Matcher<I> {
         this.matchers = matchers
     }
 
-    matches = (org: I) => this.matchers.reduce<boolean>((result, matcher) => result || matcher.matches(org), false)
+    matches = (input: I) => this.matchers.reduce<boolean>((result, matcher) => result || matcher.matches(input), false)
 }
 
-export class HeaderMatcher<I> implements Matcher<I> {
+export abstract class ValueMatcher<I> implements Matcher<I> {
+    abstract getValue(input: I, key:string): string
 
+    name: string
+    pattern: RegExp
+
+    constructor(name: string, regexp: string) {
+        this.name = name
+        this.pattern = new RegExp(regexp)
+    }
+
+    matches(input: I): boolean {
+        let headValue = this.getValue(input, this.name)
+        return this.pattern.exec(headValue) != null
+    }
 }
 
+export abstract class JsonQueryBodyMatch<I> extends ValueMatcher<I> {
+    abstract getJsonObject(input: I): object
+
+    constructor(query:string, regexp: string) {
+        super(query, regexp)
+    }
+
+    getValue(input: I, name: string) {
+        return jsonQuery(name, { data: this.getJsonObject(input) }).toString()
+    }
+}
