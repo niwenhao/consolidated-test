d9ab0937151a05270afb4a04b7cd52737ab2c9e6
Wenhao Ni <niwenhao@gmail.com>
2018-06-18 21:46:58 +0900

    u

--- src/ts/entity/response-condition.ts
+++ src/ts/entity/response-condition.ts
@@ -1,5 +1,5 @@
 import { AxiosResponse } from 'axios'
-import { Matcher } from '../http-matcher/matcher';
+import { Matcher, AndMatcher } from '../http-matcher/matcher';
 
 
 type ResMatcher = Matcher<AxiosResponse>
@@ -10,6 +10,74 @@ export const ResponseConditionFactory: { [key:string]: ConditionFactory} = {}
 ResponseConditionFactory.and = (config:any) => {
     if (config instanceof Map) {
         let matches: ResMatcher[] = []
-
+        config.forEach((value, key) => {
+            let f = ResponseConditionFactory["key"]
+            if (f) {
+                matches.push(f(value))
+            } else {
+                throw `Condition ${key} was not found`
+            }
+        })
+        return new AndMatcher(matches)
+    } else {
+		throw "And condition need some subconditions"
     }
+}
+
+ResponseConditionFactory.or = (config: any) => {
+	if (config instanceof Map) {
+		let matches = [] as ResMatcher[]
+		config.forEach((value, key) => {
+			let f = ResponseConditionFactory[key]
+			if (f) {
+				matches.push(f(value))
+			} else {
+				throw `Condition ${key} was not found`
+			}
+
+		})
+		return new OrMatcher(matches)
+	} else {
+		throw "Or condition need some subconditions"
+	}
+}
+
+ResponseConditionFactory.url = (config: any) => {
+	if (typeof config.pattern == "string") {
+		return new ExpressRequestUrlMatcher("", config.pattern)
+	} else {
+		throw "Url condition need a pattern"
+	}
+}
+
+RequestConditonFactories.query = (config: any) => {
+	if (typeof config.name == "string" && typeof config.pattern == "string") {
+		return new ExpressRequestQueryMatcher(config.name, config.pattern)
+	} else {
+		throw 'Query condition need name and pattern'
+	}
+}
+
+RequestConditonFactories.header = (config: any) => {
+	if (typeof config.name == "string" && typeof config.pattern == "string") {
+		return new ExpressRequestHeadMatcher(config.name, config.pattern)
+	} else {
+		throw 'Header condition need name and pattern'
+	}
+}
+
+RequestConditonFactories.form = (config: any) => {
+	if (typeof config.name == "string" && typeof config.pattern == "string") {
+		return new ExpressRequestBodyMatcher(config.name, config.pattern)
+	} else {
+		throw 'From condition need name and pattern'
+	}
+}
+
+RequestConditonFactories.json = (config: any) => {
+	if (typeof config.query == "string" && typeof config.pattern == "string") {
+		return new ExpressRequestJsonQueryBodyMatcher(config.query, config.pattern)
+	} else {
+		throw 'Json condition need query and pattern'
+	}
 }
